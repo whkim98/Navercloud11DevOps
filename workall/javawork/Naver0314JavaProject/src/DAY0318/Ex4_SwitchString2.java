26384ea0e6a76da4eb856ac70f19e4d8f0f2fa8e
김우형 <hyeong7272@daum.net>
2024-03-18 11:32:45 +0900

    0318

---
+++ workall/javawork/Naver0314JavaProject/src/DAY0318/Ex4_SwitchString2.java
@@ -0,0 +1,39 @@
+package DAY0318;
+
+import java.util.Scanner;
+
+public class Ex4_SwitchString2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String color = "";
+		
+		System.out.println("red, green, pink중 선택");
+		
+		color = sc.nextLine();
+		
+		switch(color) {
+		case "RED":
+			
+		case "red":
+			System.out.println("red");
+			break;
+		case "GREEN":
+			
+		case "green":
+			System.out.println("green");
+			break;
+		case "PINK":
+			
+		case "pink":
+			System.out.println("pink");
+			break;
+		default: 
+			System.out.println("잘못된 선택");
+		}
+		
+	}
+	
+}
