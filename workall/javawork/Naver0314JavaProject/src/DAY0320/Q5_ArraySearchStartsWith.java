4e6df9da093de5ed2a457882e0fb90f273a8f376
김우형 <hyeong7272@daum.net>
2024-03-20 15:23:52 +0900

    0320

---
+++ workall/javawork/Naver0314JavaProject/src/DAY0320/Q5_ArraySearchStartsWith.java
@@ -0,0 +1,57 @@
+package DAY0320;
+
+import java.util.Scanner;
+
+public class Q5_ArraySearchStartsWith {
+	
+	public static void main(String[] args) {
+		
+		String[] members = {"강호동", "한가인", "유재석", "이승민", "강하나", 
+				"손미나", "이영자", "박남정", "한지혜", "손창민"};
+		
+		/*
+		 * 검색할 이름은? 강
+		 * 1번째: 강호동
+		 * 5번째: 강하나
+		 * 총 2명 검색
+		 * 
+		 * 검색할 이름은? 박이
+		 * "박이"로 시작하는 멤버는 없습니다.
+		 * 
+		 * 검색할 이름은? Q
+		 * 검색을 종료합니다.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		String searchName = "";
+		
+		while(true) {
+			boolean w = true;
+			
+			System.out.println("검색할 이름은?");
+			searchName = sc.nextLine();
+			
+			if(searchName.equals("q") || searchName.equals("Q")) {
+				System.out.println("검색을 종료합니다.");
+				break;
+			}
+			
+			for(int i = 0; i < members.length; i++) {
+				if(members[i].startsWith(searchName) == true) {
+					w = false;
+					System.out.println((i + 1) + "번쨰: " + members[i]);
+				}
+				
+			}
+			
+			if(w == true) {
+				System.out.println(searchName + "로 시작하는 멤버는 없습니다.");
+			}
+			
+		}
+		
+		sc.close();
+		
+	}
+
+}
