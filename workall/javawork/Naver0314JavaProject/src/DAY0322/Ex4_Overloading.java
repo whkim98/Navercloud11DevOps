3ea0bbe3380513efa5450c87194c8272c8f5c801
김우형 <hyeong7272@daum.net>
2024-03-22 14:30:15 +0900

    0322

--- workall/javawork/Naver0314JavaProject/src/DAY0322/Ex4_Overloading.java
+++ workall/javawork/Naver0314JavaProject/src/DAY0322/Ex4_Overloading.java
@@ -10,7 +10,27 @@ class oper{
 	}
 	
 	static public String sum(String a, String b) {
-		return a+ b;
+		return a + b;
+	}
+	
+	//Variable Argument
+	static public int hap(int ...n) { //갯수제한이 없음
+		System.out.println("전달받은 숫자의 갯수: " + n.length);
+		int s = 0;
+		for(int i = 0; i < n.length; i++) {
+			System.out.printf("%10d", n[i]);
+			s+=n[i];
+		}
+		System.out.println();
+		return s;
+	}
+	
+	static public void writeColor(String ...color) {
+		System.out.println("총 " + color.length + "개의 색상 전달받음");
+		for(int i = 0; i < color.length; i++) {
+			System.out.println(i + ": " + color[i]);
+		}
+		System.out.println("=".repeat(20));
 	}
 }
 
@@ -18,7 +38,15 @@ public class Ex4_Overloading {
 	
 	public static void main(String[] args) {
 		
+		System.out.println(oper.sum(5, 100));
 		
+		System.out.println(oper.sum(1.2, 5.6));
+		
+		System.out.println(oper.sum("apple", "banana"));
+		
+		System.out.println(oper.hap(1, 2, 5, 100, 2000, 30000, 2212321));
+		
+		oper.writeColor("red\n", "yellow\n", "black\n", "pink\n", "brown");
 		
 	}
 
---
+++ workall/javawork/Naver0314JavaProject/src/DAY0322/Ex5_CallBy.java
@@ -0,0 +1,5 @@
+package DAY0322;
+
+public class Ex5_CallBy {
+
+}
