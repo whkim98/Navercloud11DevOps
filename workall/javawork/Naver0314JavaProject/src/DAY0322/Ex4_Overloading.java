53255ff0f2a80fe97233f5499524d0ca800fe2bc
김우형 <hyeong7272@daum.net>
2024-03-22 14:07:51 +0900

    0322

---
+++ workall/javawork/Naver0314JavaProject/src/DAY0322/Ex4_Overloading.java
@@ -0,0 +1,25 @@
+package DAY0322;
+
+class oper{
+	static public int sum(int a, int b) {
+		return a + b;
+	}
+	
+	static public double sum(double a, double b) {
+		return a + b;
+	}
+	
+	static public String sum(String a, String b) {
+		return a+ b;
+	}
+}
+
+public class Ex4_Overloading {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
