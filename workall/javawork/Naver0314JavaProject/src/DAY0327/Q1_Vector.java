8fda6bbc500977700845fa2f18a0ac9d49359527
김우형 <hyeong7272@daum.net>
2024-03-27 14:37:15 +0900

    0327

--- workall/javawork/Naver0314JavaProject/src/DAY0327/Q1_Vector.java
+++ workall/javawork/Naver0314JavaProject/src/DAY0327/Q1_Vector.java
@@ -1,11 +1,19 @@
 package DAY0327;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
 /*
- * 1. 이름추가 2. 이름삭제 3. 이름출력 4. 이름검색 5. 종료
+ * 1. 이름추가 2. 이름삭제 3. 이름출력 4. 이름검색 5. 저장 & 종료
+ * 
+ * 종료시 파일에 저장
+ * 처음 생성시 파일에서 읽어올 것
  */
 
 public class Q1_Vector {
@@ -13,6 +21,62 @@ public class Q1_Vector {
 	Scanner sc = new Scanner(System.in);
 	
 	List<String> list = new Vector<>();
+	
+	static final String FILENAME = "/Users/uhyeonge/Desktop/navercloud/workall/naver0314File/person.txt";
+	
+	public Q1_Vector() {
+		//파일을 읽어 list에 저장된 이름 추가하기
+		try {
+			personRead();
+		} catch (IOException e) {
+//			e.printStackTrace();
+			System.out.println("얽어올 명단이 없습니다");
+		}
+	}
+	
+	public void personRead() throws IOException {
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		try {
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+			while(true) {
+				String name = br.readLine();
+				if(name == null) {
+					break;
+				}
+				list.add(name);
+			}
+			System.out.println("총 " + list.size() + "명의 멤버를 파일에서 읽습니다");
+		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+			System.out.println("읽어올 명단이 없습니다");
+		}finally {
+			if(br != null) br.close();
+			if(fr != null) fr.close();
+		}
+	}
+	
+	public void personSave() {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(FILENAME);
+			for(String s: list) {
+				fw.write(s + "\n");
+			}
+			System.out.println("총 " + list.size() + "명의 명단을 저장합니다");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(fw != null)
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
 	
 	//메뉴 선택시 번호 반환
 	public int getMenu() {
@@ -84,6 +148,7 @@ public class Q1_Vector {
 		
 		if(menu == 5) {
 			System.out.println("종료합니다ㅓ");
+			v.personSave();
 			break;
 		}
 		
