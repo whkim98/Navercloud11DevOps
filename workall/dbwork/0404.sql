5a2d1a0fb30de10bd11c6409cf0a14b3ea12dc0f
김우형 <hyeong7272@daum.net>
2024-04-04 13:06:20 +0900

    0404

--- workall/dbwork/0404.sql
+++ workall/dbwork/0404.sql
@@ -100,5 +100,102 @@ select name, score, if(score>=90, '통과', '재시험') 평가 from sawon;
 
 drop table test01;
 
+select concat('happy','day') from dual; /*문자열더함*/
+select concat('apple','kiwi','mango') from dual;
+select replace('have a nice day','a','*') from dual;
+select instr('happy','a') from dual;
+select instr('happy','x') from dual;
+select left('have a nice day',4) from dual;
+select right('have a nice day',5) from dual;
+select mid('have a nice day',8,4) from dual;
+select substring('have a nice day',8,4) from dual;
+select left('가나다라마바사',3) from dual; /*한글도3글자 나옴*/
+select substring('가나다라마바사',4,2) from dual;
+select concat(ltrim('   abc    '),'*') from dual;
+select concat(rtrim('   abc    '),'*') from dual;
+select concat(trim('   abc    '),'*') from dual;
+select lcase('Happy'),lower('Happy') from dual;
+select ucase('Happy'),upper('Happy') from dual;
+select reverse('Happy') from dual;
+select abs(-10) from dual;/*무조건 양수값 반환*/
+select ceiling(2.1) from dual; /*무조건올림  3 */
+select floor(2.9) from dual; /*무조건내림  2 */
+select truncate(2.345,2) from dual; /*소숫점2자리까지 출력되고 3자리에서 내림*/
+select mod(5,3) from dual;/*5를 3으로 나눈나머지=2*/
+select greatest(12,23,34,11,4) from dual;/*가장 큰숫자*/
+select least(12,23,34,11,4) from dual;/*가장 작은숫자*/
+
 SELECT * from sawon;
+
+create table bitclass (
+   idx smallint primary key,
+   class varchar(30),
+   price int,
+   gigan smallint);
+
+create table stu (
+   num smallint auto_increment primary key,
+   name varchar(20),
+   idx smallint,
+   sugangday date,
+   constraint stu_fk_idx foreign key(idx) references bitclass(idx));
+
+-- bitclass 에 데이타 추가
+insert into bitclass values (100,'Java',110000,10);
+insert into bitclass values (200,'HTML5',90000,8);
+insert into bitclass values (300,'jQuery',130000,12);
+insert into bitclass values (400,'Oracle',180000,20);
+
+-- stu 에 데이타 추가
+insert into stu (name,idx,sugangday) values ('kim',200,now());
+insert into stu (name,idx,sugangday) values ('lee',100,now());
+insert into stu (name,idx,sugangday) values ('son',300,now());
+insert into stu (name,idx,sugangday) values ('min',400,now());
+
+select * from bitclass bc,stu where bc.idx=stu.idx;
+
+select name,class,price,gigan,sugangday from bitclass bc,stu
+  where bc.idx=stu.idx;
+
+select name,class,price,gigan,sugangday from bitclass bc 
+	inner join stu on bc.idx=stu.idx;
+
+drop table stu;
+drop table bitclass;
+
+create table shop(
+shopnum smallint auto_increment primary key,
+sangpum varchar(30),
+price int,
+color varchar(20) default 'black'
+);
+
+create table cart(
+cartnum smallint auto_increment primary key,
+username varchar(20),
+shopnum smallint,
+guipday datetime,
+constraint fk_shopnum foreign key (shopnum) references shop(shopnum) on delete cascade
+);
+
+insert into shop(sangpum, price) values('블라우스', 12000);
+insert into shop values(null, '청바지', 29000, 'blue');
+insert into shop values(null, '레이스조끼', 31000, 'yellow');
+insert into shop values(null, '주름스커트', 43000, 'green');
+insert into shop values(null, '체크남방', 32000, 'pink');
+select * from shop;
+
+insert into cart values(null, '이진', 4, now());
+insert into cart values(null, '강철수', 6, now());
+insert into cart values(null, '유재석', 1, now());
+insert into cart values(null, '김유정', 3, now());
+select * from cart;
+
+select s.shopnum, s.sangpum, s.price, s.color, c.username, c.guipday from shop s left join cart c on s.shopnum = c.shopnum;
+
+
+
+
+
+
 
