6f8c4d21911d5fd220f12049ce89a11e701ebfff
김우형 <hyeong7272@daum.net>
2024-05-14 11:23:09 +0900

    0514

--- workall/javawork/JspProject/src/main/java/data/dao/SimpleBoardDao.java
+++ workall/javawork/JspProject/src/main/java/data/dao/SimpleBoardDao.java
@@ -2,7 +2,10 @@ package data.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
 
 import data.dto.SimpleBoardDto;
 import db.common.MySqlConnect;
@@ -28,7 +31,6 @@ public class SimpleBoardDao {
 			//실행
 			pstmt.execute();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			db.dbClose(pstmt, conn);
@@ -51,11 +53,44 @@ public class SimpleBoardDao {
 			pstmt.setInt(5, dto.getNum());
 			pstmt.execute();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally {
 			db.dbClose(pstmt, conn);
 		}
+	}
+	
+	public List<SimpleBoardDto> getAllDatas(int start, int persize) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<SimpleBoardDto> list = new Vector<SimpleBoardDto>();
+		String sql = """
+				select * from simpleboard order by num desc limit ?, ?
+				""";
+		conn = db.getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, persize);
+			while(rs.next()) {
+				SimpleBoardDto dto = new SimpleBoardDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setAvata(rs.getString("avata"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setReadcount(rs.getInt("readcount"));
+				dto.setChu(rs.getInt("chu"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+				list.add(dto);
+			}
+			pstmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+		return list;
 	}
 
 }
