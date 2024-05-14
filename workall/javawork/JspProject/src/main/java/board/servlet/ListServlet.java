ace7f842fcf71a6796a828e1393808b6b18c3e3c
김우형 <hyeong7272@daum.net>
2024-05-14 11:57:33 +0900

    0514

--- workall/javawork/JspProject/src/main/java/board/servlet/ListServlet.java
+++ workall/javawork/JspProject/src/main/java/board/servlet/ListServlet.java
@@ -1,6 +1,7 @@
 package board.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,13 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListServlet
- */
+import data.dao.SimpleBoardDao;
+import data.dto.SimpleBoardDto;
+
 @WebServlet("/board/list")
 public class ListServlet extends HttpServlet {
 	
+	SimpleBoardDao dao = new SimpleBoardDao();
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int totalCount = dao.getTotalcount();
+		List<SimpleBoardDto> list = null;
+		list = dao.getAllDatas(0, 10);
+		
+		request.setAttribute("totalCount", totalCount);
+		request.setAttribute("list", list);
+		
 		RequestDispatcher rd = request.getRequestDispatcher("../day0514/boardlist.jsp");
 		rd.forward(request, response);
 	}
