f356d216963aba3d00199ce12187475e9168d11d
김우형 <hyeong7272@daum.net>
2024-05-16 11:04:51 +0900

    0516

--- workall/javawork/JspProject/src/main/java/board/servlet/UpdateChuServlet.java
+++ workall/javawork/JspProject/src/main/java/board/servlet/UpdateChuServlet.java
@@ -1,20 +1,32 @@
 package board.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import data.dao.SimpleBoardDao;
+
 /**
  * Servlet implementation class UpdateChuServlet
  */
 @WebServlet("/board/updatechu")
 public class UpdateChuServlet extends HttpServlet {
-	
+	SimpleBoardDao dao = new SimpleBoardDao();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int num = Integer.parseInt(request.getParameter("num"));
+		dao.updateChu(num);
+		int chu = dao.getData(num).getChu();
 		
+		String s = "{\"num\":"+chu+"}";
+		
+		request.setAttribute("s", s);
+		RequestDispatcher rd = request.getRequestDispatcher("../day0514/jsonchu.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
