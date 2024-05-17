866beefee935fdaef5f53de493235935e249fed4
김우형 <hyeong7272@daum.net>
2024-05-17 14:23:03 +0900

    0517

---
+++ workall/springwork/SpringMvcEx1/src/main/java/anno/study/ex4/CanadaTire.java
@@ -0,0 +1,10 @@
+package anno.study.ex4;
+
+public class CanadaTire implements Tire{
+
+	@Override
+	public String getTireName() {
+		return "캐나다타이어";
+	}
+	
+}
---
+++ workall/springwork/SpringMvcEx1/src/main/java/anno/study/ex4/KoreaTire.java
@@ -0,0 +1,10 @@
+package anno.study.ex4;
+
+public class KoreaTire implements Tire{
+
+	@Override
+	public String getTireName() {
+		return "한국타이어";
+	}
+	
+}
