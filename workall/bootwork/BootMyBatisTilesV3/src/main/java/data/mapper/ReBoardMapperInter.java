9fab9cfae2c013b7279ffcc2c1a272f4a64719a2
김우형 <hyeong7272@daum.net>
2024-05-27 14:51:47 +0900

    0527

--- workall/bootwork/BootMyBatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
@@ -1,10 +1,13 @@
 package data.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ReBoardMapperInter {
 	
-	
+	public int getMaxNum();
+	public void updateRestep(Map<String, Integer> map);
 
 }
--- workall/bootwork/BootMyBatisTilesV3/src/main/java/data/service/ReBoardService.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/data/service/ReBoardService.java
@@ -1,12 +1,28 @@
 package data.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import data.mapper.ReBoardMapperInter;
 
 @Service
 public class ReBoardService {
 
 	@Autowired
-	private ReBoardService boardMapper;
+	private ReBoardMapperInter boardMapper;
+	
+	public int getMaxNum() {
+		return boardMapper.getMaxNum();
+	}
+	
+	public void updateRestep(int regroup, int restep) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("regroup", regroup);
+		map.put("restep", restep);
+		boardMapper.updateRestep(map);
+	}
 	
 }
