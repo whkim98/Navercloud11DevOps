a93ea1d283adbe0327b6377aa05d55c5a5feb4c2
김우형 <hyeong7272@daum.net>
2024-05-27 15:22:25 +0900

    0527

--- workall/bootwork/BootMyBatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/data/mapper/ReBoardMapperInter.java
@@ -4,10 +4,13 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import data.dto.ReBoardDto;
+
 @Mapper
 public interface ReBoardMapperInter {
 	
 	public int getMaxNum();
 	public void updateRestep(Map<String, Integer> map);
+	public void insertBoard(ReBoardDto dto);
 
 }
--- workall/bootwork/BootMyBatisTilesV3/src/main/java/data/service/ReBoardService.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/data/service/ReBoardService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import data.dto.ReBoardDto;
 import data.mapper.ReBoardMapperInter;
 
 @Service
@@ -23,6 +24,29 @@ public class ReBoardService {
 		map.put("regroup", regroup);
 		map.put("restep", restep);
 		boardMapper.updateRestep(map);
+	}
+	
+	public void insertBoard(ReBoardDto dto) {
+		int num = dto.getNum();
+		int regroup = dto.getRegroup();
+		int restep = dto.getRestep();
+		int relevel = dto.getRelevel();
+		
+		if(num == 0) {
+			regroup = this.getMaxNum() + 1;
+			restep = 0;
+			relevel = 0;
+		}else {
+			this.updateRestep(regroup, restep);
+			restep+=1;
+			relevel+=1;
+		}
+		
+		dto.setRegroup(regroup);
+		dto.setRestep(restep);
+		dto.setRelevel(relevel);
+		
+		boardMapper.insertBoard(dto);
 	}
 	
 }
