7b38f13328d7ca811714d3de6599bb57b9da9f20
김우형 <hyeong7272@daum.net>
2024-05-28 11:49:21 +0900

    0528

--- workall/bootwork/BootMyBatisTilesV3/src/main/java/controller/board/BoardDetailDeleteController.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/controller/board/BoardDetailDeleteController.java
@@ -2,7 +2,12 @@ package controller.board;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import data.dto.ReBoardDto;
+import data.service.MemberService;
 import data.service.ReBoardService;
 
 @Controller
@@ -10,5 +15,22 @@ public class BoardDetailDeleteController {
 	
 	@Autowired
 	private ReBoardService boardService;
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping("/board/detail")
+	public String detail(@RequestParam int num, @RequestParam int currentPage, Model model) {
+		
+		ReBoardDto dto = boardService.getData(num);
+		
+		String profile_photo = memberService.getDataById(dto.getMyid()).getPhoto();
+		
+		model.addAttribute("dto", dto);
+		model.addAttribute("profile_photo", profile_photo);
+		model.addAttribute("currentPage", currentPage);
+		
+		return "board/detailpage";
+	}
 
 }
