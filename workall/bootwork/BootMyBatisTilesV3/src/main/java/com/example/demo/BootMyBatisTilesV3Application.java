7f8b1cb65dea581a004d291200905eefc7ad1fbf
김우형 <hyeong7272@daum.net>
2024-05-29 16:07:20 +0900

    0529

--- workall/bootwork/BootMyBatisTilesV3/src/main/java/com/example/demo/BootMyBatisTilesV3Application.java
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/com/example/demo/BootMyBatisTilesV3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"controller.*", "*.member", "data.*", "tiles.config", "com.example.demo"})
+@ComponentScan({"controller.*", "*.member", "data.*", "tiles.config", "com.example.demo", "naver.cloud"})
 @MapperScan("data.mapper")
 public class BootMyBatisTilesV3Application {
 
