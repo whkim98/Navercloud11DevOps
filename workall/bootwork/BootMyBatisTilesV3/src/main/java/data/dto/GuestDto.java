de34d956d4cfcfbf1eeb33e9052293c73c48da7c
김우형 <hyeong7272@daum.net>
2024-05-30 11:15:29 +0900

    0530

---
+++ workall/bootwork/BootMyBatisTilesV3/src/main/java/data/dto/GuestDto.java
@@ -0,0 +1,28 @@
+package data.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Alias("GuestDto")
+public class GuestDto {
+	
+	private int guestidx;
+	private String writer;
+	private String myid;
+	private String gcontent;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+	private Timestamp writeday;
+
+}
