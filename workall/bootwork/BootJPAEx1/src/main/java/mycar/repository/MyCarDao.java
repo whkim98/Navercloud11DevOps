2a3677aba53612d118c6d66a68e3de3b7988c218
김우형 <hyeong7272@daum.net>
2024-07-04 10:42:15 +0900

    0704

--- workall/bootwork/BootJPAEx1/src/main/java/mycar/repository/MyCarDao.java
+++ workall/bootwork/BootJPAEx1/src/main/java/mycar/repository/MyCarDao.java
@@ -23,8 +23,8 @@ public class MyCarDao{
     //전체목록
     public List<MycarDto> getAllCars()
     {
-        return daoInter.findAll(); //추가된 순서로 반환
-        //return daoInter.findAll(Sort.by(Sort.Direction.DESC,"carprice"));//가격이 비싼거부터 출력
+//        return daoInter.findAll(); //추가된 순서로 반환
+        return daoInter.findAll(Sort.by(Sort.Direction.DESC,"carprice"));//가격이 비싼거부터 출력
         //return daoInter.findAll(Sort.by(Sort.Direction.ASC,"carname"));//자동차명의 오름차순
     }
 }
