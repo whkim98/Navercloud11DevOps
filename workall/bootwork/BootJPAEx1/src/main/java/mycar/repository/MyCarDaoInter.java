af9caeb62694a0bb335529b0dba60fbc587d9f60
김우형 <hyeong7272@daum.net>
2024-07-04 12:02:15 +0900

    0704

--- workall/bootwork/BootJPAEx1/src/main/java/mycar/repository/MyCarDaoInter.java
+++ workall/bootwork/BootJPAEx1/src/main/java/mycar/repository/MyCarDaoInter.java
@@ -1,9 +1,20 @@
 package mycar.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import mycar.data.MycarDto;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MyCarDaoInter extends JpaRepository<MycarDto, Long>{
-
+    @Query(value= """
+    update mycar set carname=:carname, carprice=:carprice, carcolor=:carcolor
+    where num=:num
+""", nativeQuery=true)
+    @Modifying
+    @Transactional
+    public void updateMycarNoPhoto(@Param("num") Long num, @Param("carname") String carname, @Param("carprice") int carprice,
+    @Param("carcolor") String carcolor);
 }
