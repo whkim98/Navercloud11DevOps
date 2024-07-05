c9a7d0694f95aae64b41a4eb6048d9ac566d598b
김우형 <hyeong7272@daum.net>
2024-07-05 11:58:42 +0900

    0705

---
+++ workall/nodework/ex3.js
@@ -0,0 +1,14 @@
+const PI = 3.141592;
+// PI = 2.345;
+let r = 3;
+let area = r * r * PI;
+
+console.log("반지름이 %d인 원넓이: %f", r, area);
+
+const pp = {
+    "name": "홍길동",
+    "addr": "서울"
+}
+
+console.log("이름: " + pp.name);
+console.log("주소: " + pp.addr);
