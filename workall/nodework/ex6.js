e2ea5cf2550852ee4a18c3bdfeb50091c5897bcf
김우형 <hyeong7272@daum.net>
2024-07-05 14:30:01 +0900

    0705

--- workall/nodework/ex6.js
+++ workall/nodework/ex6.js
@@ -11,3 +11,16 @@ console.log(obj2.addr);
 let name1 = obj1.name;
 let age1 = obj1.age;
 console.log(name1, age1);
+
+let {name, age} = obj1;
+console.log("es6 name" + name);
+console.log("es6 age" + age);
+
+function f1({name = "수지", age = 22} = {}){
+    console.log("name" + name);
+    console.log("age" + age);
+}
+
+f1();
+f1(obj1);
+
