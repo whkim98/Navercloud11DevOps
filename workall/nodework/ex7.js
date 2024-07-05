549d12d903d4c4b8deb66b2b5d07beb25270815c
김우형 <hyeong7272@daum.net>
2024-07-05 14:33:37 +0900

    0705

---
+++ workall/nodework/ex7.js
@@ -0,0 +1,7 @@
+let a = 12;
+let b = 23.0;
+let c = 12.6;
+
+console.log(a + "는 정수임?" + Number.isInteger(a));
+console.log(b + "는 정수임?" + Number.isInteger(b));
+console.log(c + "는 정수임?" + Number.isInteger(c));
