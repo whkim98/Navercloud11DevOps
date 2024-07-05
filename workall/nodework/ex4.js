270d958e8a4da16deda2693a14ac8fe3172e5f0e
김우형 <hyeong7272@daum.net>
2024-07-05 12:09:11 +0900

    0705

--- workall/nodework/ex4.js
+++ workall/nodework/ex4.js
@@ -1,7 +1,13 @@
-function func(x, y = 100, z = 200){
-    console.log(x,y,z);
-}
+// function func(x, y = 100, z = 200){
+//     console.log(x,y,z);
+// }
+
+let func = (x, y = 100, z = 200) => console.log(x, y, z);
+
+let add = (a, b) => a + b;
 
 func(4);
 func(5, 6);
 func(1, 2, 3);
+
+console.log(add(3, 5));
