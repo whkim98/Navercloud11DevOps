a7eb274dc5c715098a33495bdf96e5c6c323fff9
김우형 <hyeong7272@daum.net>
2024-07-05 12:23:20 +0900

    0705

--- workall/nodework/ex4.js
+++ workall/nodework/ex4.js
@@ -6,8 +6,11 @@ let func = (x, y = 100, z = 200) => console.log(x, y, z);
 
 let add = (a, b) => a + b;
 
+let mul = (a, b) => a * b;
+
 func(4);
 func(5, 6);
 func(1, 2, 3);
 
 console.log(add(3, 5));
+console.log(mul(3, 5));
---
+++ workall/nodework/ex5.js
@@ -0,0 +1,23 @@
+let arr = [3, 5, 7];
+let f1 = (a, b) => console.log(a, b);
+
+f1(arr[0], arr[1]);
+f1(...arr);
+
+let arr1 = [4, 6];
+let arr2 = [10, 20, 30];
+let arr3 = [3, 4, ...arr1, 15, ...arr2];
+
+console.log(arr3.length);
+console.log(arr3);
+
+let sum = 0;
+for(let su of arr3){
+    sum += su;
+}
+
+console.log(sum);
+
+let [a, ...b] = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(a);
+console.log(b);
