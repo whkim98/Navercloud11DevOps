1d1d98cd07942811e7441737abf7e1cb7f4bd3de
김우형 <hyeong7272@daum.net>
2024-07-08 17:23:51 +0900

    0708

--- workall/reactwork/react-first/src/components/Main.js
+++ workall/reactwork/react-first/src/components/Main.js
@@ -1,12 +1,15 @@
 import FiveApp from "./FiveApp";
 import FourApp from "./FourApp";
 import { OneApp } from "./OneApp"
+import SixApp from "./SixApp";
 import ThreeApp from "./ThreeApp";
 import TwoApp from "./TwoApp";
 
 const Main =()=>{
     return (
         <div>
+            <SixApp/>
+            <hr/>
             <FiveApp/>
             <hr/>
             <FourApp/>
