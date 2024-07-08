3fed286bb11c91b2472f08a89fb578a298abfeae
김우형 <hyeong7272@daum.net>
2024-07-08 17:42:25 +0900

    0708

--- workall/reactwork/react-first/src/components/Main.js
+++ workall/reactwork/react-first/src/components/Main.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FiveApp from "./FiveApp";
 import FourApp from "./FourApp";
 import { OneApp } from "./OneApp"
@@ -6,9 +7,10 @@ import ThreeApp from "./ThreeApp";
 import TwoApp from "./TwoApp";
 
 const Main =()=>{
+    const [idx,setIdx]=useState(6);
     return (
         <div>
-            <SixApp/>
+            {/* <SixApp/>
             <hr/>
             <FiveApp/>
             <hr/>
@@ -19,7 +21,23 @@ const Main =()=>{
             <TwoApp/>
             <hr/>
             <OneApp/>
-            <hr/>
+            <hr/> */}
+            <h1>2024-07-08 리액트 첫수업</h1>
+            <select className="form-select" style={{width:'200px'}}
+            onChange={(e)=>setIdx(Number(e.target.value))}>
+                <option value={1}>1번 컴포넌트</option>
+                <option value={2}>2번 컴포넌트</option>
+                <option value={3}>3번 컴포넌트</option>
+                <option value={4}>4번 컴포넌트</option>
+                <option value={5}>5번 컴포넌트</option>
+                <option value={6} selected>오늘의 문제</option>                
+            </select>
+            <br/><br/>
+            {
+                idx===1?<OneApp/>:idx===2?<TwoApp/>:
+                idx===3?<ThreeApp/>:idx===4?<FourApp/>:
+                idx===5?<FiveApp/>:<SixApp/>
+            }
         </div>
     )
 }
