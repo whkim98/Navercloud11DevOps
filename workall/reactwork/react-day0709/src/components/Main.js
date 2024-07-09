d295009f5bc525c2207951aac14e3a80c436297d
김우형 <hyeong7272@daum.net>
2024-07-09 10:24:19 +0900

    0709

--- workall/reactwork/react-day0709/src/components/Main.js
+++ workall/reactwork/react-day0709/src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OneApp from './OneApp';
 import TwoApp from './TwoApp';
 import ThreeApp from './ThreeApp';
@@ -7,20 +7,25 @@ import FiveApp from './FiveApp';
 import SixApp from './SixApp';
 
 const Main = () => {
+    const [idx, setIdx] = useState(1);
     return (
         <div>
-            <OneApp/>
-            <hr></hr>
-            <TwoApp/>
-            <hr/>
-            <ThreeApp/>
-            <hr/>
-            <FourApp/>
-            <hr/>
-            <FiveApp/>
-            <hr/>
-            <SixApp/>
-            <hr/>
+            <h3>2024-07-09</h3>
+            <br/>
+            <select className='form-select' style={{width: '300px'}}
+            onChange={(e)=>setIdx(Number(e.target.value))}>
+                <option value={1}>OneApp-styled-components</option>
+                <option value={2}>TwoApp</option>
+                <option value={3}>ThreeApp</option>
+                <option value={4}>FourApp</option>
+                <option value={5}>FiveApp</option>
+                <option value={6}>SixApp</option>
+            </select>
+            <br/>
+            {
+                idx === 1 ? <OneApp/> : idx === 2 ? <TwoApp/> : idx === 3 ? <ThreeApp/> :
+                idx === 4 ? <FourApp/> : idx === 5 ? <FiveApp/> : <SixApp/>
+            }
         </div>
     );
 };
