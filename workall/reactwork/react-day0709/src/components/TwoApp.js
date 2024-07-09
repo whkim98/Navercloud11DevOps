82639c3a2f9fccbb9115566bf679e5d91886670f
김우형 <hyeong7272@daum.net>
2024-07-09 12:32:13 +0900

    0709

--- workall/reactwork/react-day0709/src/components/TwoApp.js
+++ workall/reactwork/react-day0709/src/components/TwoApp.js
@@ -2,41 +2,52 @@ import React, { useState } from 'react';
 import photo1 from '../image/17.jpg';
 
 const TwoApp = () => {
-    const [photo, setPhoto]=useState('18');
+    const [photo,setPhoto]=useState('18');
+
+    //radio event
     const radioClick=(e)=>{
         setPhoto(e.target.value);
     }
+
     return (
         <div>
-            <h3 className='alert alert-danger'>TwoApp-이미지</h3>
-            <h5>import로 이미지</h5>
-            <img alt="photo1" src={photo1} style={{width: '100px'}}></img>
+            <h3 className='alert alert-danger'>TwoApp-이미지를 불러오는 또다른 방법</h3>
+            <h5>import 를 이용해서 이미지 가져오기</h5>
+            <img alt="photo1" src={photo1} style={{width:'100px'}}/>
 
-            <h5>require로 이미지</h5>
-            <img alt="photo2" src={require(`../image/19.jpg`)} style={{width: '100px'}}/>
+            <h5>require 를 이용해서 이미지 가져오기</h5>
+            <img alt="photo2" src={require(`../image/19.jpg`)} 
+            style={{width:'100px'}}/>
             <hr/>
-
-            <h5>radio 이미지</h5>
+            <h5>radio 를 이용해서 이미지 나타내기</h5>
             <label>
-                <input type="radio" name="photo" defaultValue={'17'} onclick={radioClick}/>김우형
+                <input type='radio' name='photo' 
+                defaultValue={'17'}
+                onClick={radioClick}/>신민아
             </label>
             &nbsp;
             <label>
-                <input type="radio" name="photo" defaultValue={'18'} defaultChecked onclick={radioClick}/>강동원
+                <input type='radio' name='photo' 
+                defaultValue={'18'} defaultChecked
+                onClick={radioClick}/>신세경
             </label>
             &nbsp;
             <label>
-                <input type="radio" name="photo" defaultValue={'19'} onclick={radioClick}/>유해진
+                <input type='radio' name='photo' 
+                defaultValue={'20'}
+                onClick={radioClick}/>유아인
             </label>
             &nbsp;
             <label>
-                <input type="radio" name="photo" defaultValue={'20'} onclick={radioClick}/>강호동
+                <input type='radio' name='photo' 
+                defaultValue={'19'}
+                onClick={radioClick}/>설현
             </label>
-            <br/>
+            <br/><br/>
             <img alt="photo" 
-            src={require(`../image/${photo}.jpg`)} 
-            style={{width: '300px', marginLeft: '100px'}}/>
-        </div>
+            src={require(`../image/${photo}.jpg`)}
+            style={{width:'200px',marginLeft:'100px'}}/>
+         </div>
     );
 };
 
