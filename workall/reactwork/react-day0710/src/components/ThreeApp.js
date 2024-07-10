82aa569def5cd2b2cbf00ce941a3e6eab199233f
김우형 <hyeong7272@daum.net>
2024-07-10 12:20:53 +0900

    0710

--- workall/reactwork/react-day0710/src/components/ThreeApp.js
+++ workall/reactwork/react-day0710/src/components/ThreeApp.js
@@ -1,10 +1,15 @@
 import { Alert } from '@mui/material';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const ThreeApp = () => {
+    const [count, setCount] = useState(1);
+    const countRef = useRef(1);
+    console.log("랜더링 중");
     return (
         <div>
-            <Alert>ThreeApp - ㄴ</Alert>
+            <Alert>ThreeApp - useRef 공부</Alert>
+            <h1>state 변수: {count}</h1>
+            <h1>ref 변수: {countRef.current}</h1>
         </div>
     );
 };
