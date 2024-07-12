da4216510cec26d08368c246bc89c27a70cb51ea
김우형 <hyeong7272@daum.net>
2024-07-12 16:23:15 +0900

    0712

--- workall/reactwork/react-day0711/src/components/FiveApp.js
+++ workall/reactwork/react-day0711/src/components/FiveApp.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { Alert } from '@mui/material';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const FiveApp = () => {
+    const [mycarlist, setMyCarList] = useState([]);
+    //백에서 마이카 목록 가져오기
+    const list = () => {
+        Axios.get("/mycar/list")
+        .then(res => {
+            setMyCarList(res.data);
+        })
+    }
+
+    //첫 로딩시 목록 가져오기
+    useEffect(() => {
+        list();
+    }, []); //[]적으면 처음 한 번만 호출
+
     return (
         <div>
-            
+            <Alert>FiveApp-Axios db데이터 가져오기</Alert>
+            <Alert severity='info'>총 {mycarlist.length}개의 자동차 정보가 있습니다</Alert>
         </div>
     );
 };
