b1587617ade0d4b8062dcb3a34e32bcfe86bf401
김우형 <hyeong7272@daum.net>
2024-07-12 15:15:17 +0900

    0712

---
+++ workall/bootwork/BootJPAReact/src/test/java/com/example/bootjpareact/BootJpaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootjpareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
