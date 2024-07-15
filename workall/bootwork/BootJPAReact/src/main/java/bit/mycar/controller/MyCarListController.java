3672936919156fd737d50057bf32aa3f664ff955
김우형 <hyeong7272@daum.net>
2024-07-15 10:07:50 +0900

    0715

--- workall/bootwork/BootJPAReact/src/main/java/bit/mycar/controller/MyCarListController.java
+++ workall/bootwork/BootJPAReact/src/main/java/bit/mycar/controller/MyCarListController.java
@@ -5,9 +5,13 @@ import bit.mycar.service.MyCarService;
 import lombok.RequiredArgsConstructor;
 import naver.storage.NcpObjectStorageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,11 @@ public class MyCarListController {
     @GetMapping("/mycar/list")
     public List<MycarDto> list(){
         return myCarService.getAllDatas();
+    }
+
+    @PostMapping("/mycar/upload")
+    public Map<String, String> uploadPhoto(@RequestParam("upload") MultipartFile upload){
+
     }
 
 }
