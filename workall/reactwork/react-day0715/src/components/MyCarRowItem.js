b317dbf38fb93b57d0ffbbfeaa0ca8d4b92f8a5b
김우형 <hyeong7272@daum.net>
2024-07-15 15:55:30 +0900

    0715

--- workall/reactwork/react-day0715/src/components/MyCarRowItem.js
+++ workall/reactwork/react-day0715/src/components/MyCarRowItem.js
@@ -1,11 +1,21 @@
 import { DeleteForeverOutlined } from '@mui/icons-material';
 import axios from 'axios';
+import Button from '@mui/material/Button';
 import React, { useState } from 'react';
 import MyCarList from './MyCarList';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
 
 const MyCarRowItem = ({idx, row, onDelete}) => {
     const photopath1 = "https://ph9dc3ti3779.edge.naverncp.com/PxOjfBhSlM/mycar";
     const photopath2 = "?type=f&w=40&h=40&faceopt=true&ttype=jpg";
+
+    const storagepath = "https://kr.object.ncloudstorage.com/bitcamp-whkim-123/mycar"
 
     // const [num, setNum] = useState(0);
     const deleteDataEvent = (num) => {
@@ -15,12 +25,47 @@ const MyCarRowItem = ({idx, row, onDelete}) => {
         }
     }
 
+    // const Transition = React.forwardRef(function Transition(props, ref) {
+    //     return <Slide direction="up" ref={ref} {...props} />;
+    //   });
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <tr style={{fontSize: '14px'}}>
             <td>
                 <img alt="photo" src={`${photopath1}/${row.carphoto}${photopath2}`}
                 border='1'/>
                 <span style={{marginLeft: '5px'}}>{row.carname}</span>
+                <Dialog
+                    open={open}
+                    // TransitionComponent={Transition}
+                    keepMounted
+                    onClose={handleClose}
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        <img alt="photo" src={`${storagepath}/${row.carphoto}`} style={{maxWidth: '100px'}}/>
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose}>닫기</Button>
+                    <Button onClick={handleClose}>Agree</Button>
+                    </DialogActions>
+                </Dialog>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                    자세히보기
+                </Button>
             </td>
             <td align='right'>{row.carprice}만원</td>
             <td>
@@ -29,6 +74,7 @@ const MyCarRowItem = ({idx, row, onDelete}) => {
             </td>
             <td>{row.carguip}</td>
             <td>
+                
                 <span style={{color: 'gray', fontSize: '13px'}}>
                     {row.writeday}
                     &nbsp;
@@ -37,6 +83,7 @@ const MyCarRowItem = ({idx, row, onDelete}) => {
                 </span>
             </td>
         </tr>
+        
     );
 };
 
