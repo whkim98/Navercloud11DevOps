b0a0555b0e43e8a2a7ec369e0485d7454caf22e0
김우형 <hyeong7272@daum.net>
2024-07-16 11:28:10 +0900

    0716

--- workall/reactwork/react-day0716router/src/RouterMain.js
+++ workall/reactwork/react-day0716router/src/RouterMain.js
@@ -4,23 +4,25 @@ import { Route, Routes } from 'react-router-dom';
 import { About, Food, Home } from './pages';
 import photo1 from './image/s5.JPG';
 import photo2 from './image/s2.JPG';
+import './components/MyStyle.css';
 
 const RouterMain = () => {
     return (
         <div>
             <Menu/>
-            <hr style={{clear: 'both'}}/>
+            <hr style={{clear:'both'}}/>
             <Routes>
-                <Route path='/' element={<Home/>}></Route>
-
+                <Route path='/' element={<Home/>}/>
+                {/* <Route path='/food' element={<Food/>}/> */}
 
                 <Route path='/food' element={<Food/>}>
                     <Route path=':food1' element={<Food/>}/>
                     <Route path=':food1/:food2' element={<Food/>}/>
                 </Route>
 
-
                 <Route path='/about' element={<About/>}/>
+
+                {/* 직접 태그를 이용해서 페이지를 구현해도 된다 */}
                 <Route path='/login/*' element={
                     <div>
                         <h2>로그인 메뉴입니다</h2>
@@ -28,12 +30,15 @@ const RouterMain = () => {
                         <About/>
                     </div>
                 }/>
+
+                {/* 이외의 모든 매핑주소에 대한 처리 */}
                 <Route path='*' element={
                     <div>
                         <h2>잘못된 페이지입니다</h2>
-                        <img src={photo2} alt=''></img>
+                        <img alt='' src={photo2}/>
                     </div>
                 }/>
+
             </Routes>
         </div>
     );
--- workall/reactwork/react-day0716router/src/components/Menu.js
+++ workall/reactwork/react-day0716router/src/components/Menu.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './MyStyle.css';
+import "./MyStyle.css";
 
 const Menu = () => {
     return (
         <div>
-            
             <ul className='menu'>
                 <li>
                     <NavLink to={'/'}>Home</NavLink>
@@ -14,19 +13,19 @@ const Menu = () => {
                     <NavLink to={'/food'}>Food</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'/food/11'}>Food#1</NavLink>
+                    <NavLink to={'/food/11'}>Food #1</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'/food/2/4'}>Food#2</NavLink>
+                    <NavLink to={'/food/2/4'}>Food #2</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'about'}>About</NavLink>
+                    <NavLink to={'/about'}>About</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'about/Samsung'}>About#1</NavLink>
+                    <NavLink to={'/about/Samsung'}>About #2</NavLink>
                 </li>
                 <li>
-                    <NavLink to={'about/Naver'}>About#2</NavLink>
+                    <NavLink to={'/about/Naver'}>About #3</NavLink>
                 </li>
             </ul>
         </div>
