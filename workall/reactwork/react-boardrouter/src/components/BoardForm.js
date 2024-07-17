491920281485eb04310ea5974208a055f4ac87d3
김우형 <hyeong7272@daum.net>
2024-07-17 14:28:52 +0900

    0717

--- workall/reactwork/react-boardrouter/src/components/BoardForm.js
+++ workall/reactwork/react-boardrouter/src/components/BoardForm.js
@@ -5,7 +5,7 @@ import InputEmojiWithRef from 'react-input-emoji';
 
 const BoardForm = () => {
 
-    const [writer, serWriter] = useState('');
+    const [writer, setWriter] = useState('');
     const [pass, setPass] = useState('');
     const [subject, setSubject] = useState('');
     const [photo, setPhoto] = useState("no");
@@ -17,11 +17,13 @@ const BoardForm = () => {
     console.log(storage);
 
     const uploadPhoto = (e) => {
-
+        const uploadFile = e.target.files[0];
+        const uploadForm = new FormData();
+        uploadForm.append("upload", uploadFile);
     }
 
     const dataSaveEvent = () => {
-        
+
     }
 
     return (
@@ -32,13 +34,15 @@ const BoardForm = () => {
                     <tr>
                         <th className='table-info' style={{width: '100px'}}>작성자</th>
                         <td>
-                            <input type='text' className='form-control'/>
+                            <input type='text' className='form-control' value={writer}
+                            onChange={(e) => setWriter(e.target.value)}/>
                         </td>
                     </tr>
                     <tr>
                         <th className='table-info' style={{width: '100px'}}>비밀번호</th>
                         <td>
-                            <input type='password' className='form-control'/>
+                            <input type='password' className='form-control' value={pass}
+                            onChange={(e) => setPass(e.target.value)}/>
                         </td>
                     </tr>
                     <tr>
@@ -48,14 +52,19 @@ const BoardForm = () => {
                             cleanOnEnter
                             onEnter={(text) => {
                                 console.log(text);
+                                setSubject(text);
                             }}/>
                         </td>
                     </tr>
                     <tr>
                         <td colSpan={2}>
-                            <input type='file' style={{display: 'none'}} ref={fileRef}/>
+                            <input type='file' style={{display: 'none'}} ref={fileRef}
+                            onChange={uploadPhoto}/>
                             <CameraAltOutlined onClick={()=>fileRef.current.click()} 
                             style={{cursor: 'pointer', fontSize: '30px'}}/>
+
+                            <img alt="" src={`${storage}/${photo}`} style={{width: '60px', marginLeft: '30px'}}/>
+
                             <textarea style={{width: '100%', height: '150px'}} 
                             placeholder='내용을 입력해 주세요' className='form-control'>
                             </textarea>
@@ -64,7 +73,8 @@ const BoardForm = () => {
                     <tr>
                         <td colSpan={2} align='center'>
                             <Button variant='contained' color='success'
-                            style={{width: '100px'}}>저장</Button>
+                            style={{width: '100px'}}
+                            onClick={dataSaveEvent}>저장</Button>
                         </td>
                     </tr>
                 </tbody>
